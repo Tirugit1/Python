be61b6f0b52bec1169004f51a92f9e342dcef060
Tirugit1 <tirugit1@gmail.com>
2025-11-07 11:32:50 +0530

    Windows_EC2.1

--- main.tf
+++ main.tf
@@ -1,39 +1,11 @@
 provider "aws" {
-    region = "ap-south-1"
+  region = "ap-south-1"
 }
-  variable "cidr" {
-    description = "CIDR block for security group"
-    type = string
+resource "aws_instance" "Mytest" {
+  ami           = "ami-06fa3f12191aa3337" # Replace with a valid AMI ID for your region
+  instance_type = "t3.micro"
+  key_name      = "TridevKey"
+  tags = {
+    Name = "Tiru"
   }
-resource "aws_instance" "example_instance" {
-    ami = "ami-00af95fa354fdb788"
-    instance_type = "t2.micro" 
-    
-    key_name = "TridevKey"
-
-    vpc_security_group_ids =  [aws_security_group.example_sg.id]
-      
-      root_block_device {
-        volume_size = 8
-      }
-      tags = {
-        Name = "TridevInsatance"
-      }
-    }
-    resource "aws_security_group" "example_sg" {
-      name = "example_security_group"
-      description = "Allow SSH and HTTP access"
-      ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = [var.cidr]
-    }
-    ingress{
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = [var.cidr]
-        }
-        
-        }
+}
